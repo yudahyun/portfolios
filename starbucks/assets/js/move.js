eaa177a6fed7cfbbca0381ea18a3541e8acbb10d
dahyun <ekgus1129@gmail.com>
2022-04-14 15:23:12 +0900

    수정

--- starbucks/assets/js/move.js
+++ starbucks/assets/js/move.js
@@ -28,10 +28,31 @@ document.querySelectorAll(".sub-menu").forEach((v, i) => {
   });
 });
 
+//스크롤 위치 감지하여 애니매이션 시작 
 window.addEventListener("scroll",e=>{
 
   // const documentHeight = document.scrollHeight;
-  const scrollTop = window.scrollY;
-  scro
+  // const scrollTop = window.scrollY;
+  // scro
 
+});
+
+//버튼 배경 색 바꾸기 
+document.querySelectorAll(".border_btn").forEach((v, i) => {
+  v.addEventListener("mouseover", e => {
+      const current = e.currentTarget;
+      // console.log(current);
+      const bgColor = current.dataset.color;
+      // console.log(bgColor);
+      current.style.backgroundColor = bgColor;
+      current.style.color = "#fff";
+
+  });
+  v.addEventListener("mouseout", e => {
+      const current = e.currentTarget;
+      const bgColor = current.dataset.color;
+      current.style.background = "none";
+      current.style.color = current.dataset.color;
+
+  });
 });
