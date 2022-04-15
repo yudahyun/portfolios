9e675357c614ac4718ce1810d8d77f81fd485a4d
dahyun <ekgus1129@gmail.com>
2022-04-15 12:16:11 +0900

    수정

--- starbucks/assets/js/move.js
+++ starbucks/assets/js/move.js
@@ -17,32 +17,53 @@ document.querySelectorAll(".sub-menu").forEach((v, i) => {
     const sub = e.currentTarget.children[1];
     // sub.style.display="block";
     sub.style.height = "auto";
-    sub.style.transition = "all 5s";
+    // sub.style.transition = "all 5s";
   });
   v.addEventListener("mouseout", (e) => {
     const sub = e.currentTarget.children[1];
     sub.style.height = "0";
-    sub.style.transition = "all 5s";
+    // sub.style.transition = "all 5s";
     // sub.style.display="none";
     // sub.style.height="0";
   });
 });
 
 //검색 버튼 누르면 검색 필드 나타남
-document.querySelector(".search-btn").addEventListener("click", (e) => {
-  const searchField = document.querySelector("#searchField");
-  searchField.style.width = "173px";
-  if (searchField.style.width == "173px") {
-    if (searchField.value == "") {
-      alert("검색어를 입력하세요");
-    }
-  }
-});
+document.querySelector(".search-btn").addEventListener(
+  "click",
+  (e) => {
+    const searchField = document.querySelector("#searchField");
+    searchField.style.width = "173px";
+
+    document.querySelector(".search-btn").addEventListener("click", (e) => {
+      if (searchField.style.width == "173px") {
+        if (searchField.value == "") {
+          alert("검색어를 입력하세요");
+        }
+      }
+    });
+  },
+  { once: true }
+);
+
 //스크롤 위치 감지하여 애니매이션 시작
 window.addEventListener("scroll", (e) => {
   // const documentHeight = document.scrollHeight;
   // const scrollTop = window.scrollY;
   // scro
+});
+//애니메이션 클래스 추가 함수
+function addFadein(element){
+  element.classList.add("animate__fadeIn");
+  element.style.display=("block");
+  return element;
+}
+
+document.querySelectorAll(".show-main *").forEach((v,i)=>{
+  // console.log(v);
+   const fadeOrder = v.dataset.fade ;
+   console.log(fadeOrder);
+  setTimeout(()=>{addFadein(v)}, fadeOrder * 700);
 });
 
 //버튼 배경 색 바꾸기
