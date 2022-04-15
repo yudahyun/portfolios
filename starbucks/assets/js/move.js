85a6d4c1b512cccf0743fb54584ceb91dfda1c7a
dahyun <ekgus1129@gmail.com>
2022-04-15 11:36:34 +0900

    수정

--- starbucks/assets/js/move.js
+++ starbucks/assets/js/move.js
@@ -28,39 +28,46 @@ document.querySelectorAll(".sub-menu").forEach((v, i) => {
   });
 });
 
-//스크롤 위치 감지하여 애니매이션 시작 
-window.addEventListener("scroll",e=>{
-
+//검색 버튼 누르면 검색 필드 나타남
+document.querySelector(".search-btn").addEventListener("click", (e) => {
+  const searchField = document.querySelector("#searchField");
+  searchField.style.width = "173px";
+  if (searchField.style.width == "173px") {
+    if (searchField.value == "") {
+      alert("검색어를 입력하세요");
+    }
+  }
+});
+//스크롤 위치 감지하여 애니매이션 시작
+window.addEventListener("scroll", (e) => {
   // const documentHeight = document.scrollHeight;
   // const scrollTop = window.scrollY;
   // scro
-
 });
 
-//버튼 배경 색 바꾸기 
+//버튼 배경 색 바꾸기
 document.querySelectorAll(".border_btn").forEach((v, i) => {
-  v.addEventListener("mouseover", e => {
-      const current = e.currentTarget;
-      // console.log(current);
-      const bgColor = current.dataset.color;
-      // console.log(bgColor);
-      current.style.backgroundColor = bgColor;
-      current.style.color = "#fff";
-      current.style.textDecoration="underline";
-      //혼자 검정색으로 변함
-      if(current.classList.contains("black-btn")){
-        // console.log("넌 검정");
-        current.style.color = "#000"
-      }
+  v.addEventListener("mouseover", (e) => {
+    const current = e.currentTarget;
+    // console.log(current);
+    const bgColor = current.dataset.color;
+    // console.log(bgColor);
+    current.style.backgroundColor = bgColor;
+    current.style.color = "#fff";
+    current.style.textDecoration = "underline";
+    //혼자 검정색으로 변함
+    if (current.classList.contains("black-btn")) {
+      // console.log("넌 검정");
+      current.style.color = "#000";
+    }
   });
-  v.addEventListener("mouseout", e => {
-      const current = e.currentTarget;
-      const bgColor = current.dataset.color;
-      current.style.background = "none";
-      current.style.color = current.dataset.color;
-      current.style.textDecoration="none"
-    
+  v.addEventListener("mouseout", (e) => {
+    const current = e.currentTarget;
+    const bgColor = current.dataset.color;
+    current.style.background = "none";
+    current.style.color = current.dataset.color;
+    current.style.textDecoration = "none";
   });
 });
 
-
+//
